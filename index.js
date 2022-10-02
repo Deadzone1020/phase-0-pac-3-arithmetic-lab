f1730aa081ba79bb81a75937d454911023f8ef55
Jesus Ortiz <jesusestiff@gmail.com>
2022-10-02 15:18:15 -0600

    commit tree 263920b6a60da9076b5f4ed617c6ba7c90845249

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){ 
+
+    return a + b
+}  
+
+function subtract(a,b){
+
+        return a - b 
+
+}
+
+function multiply(a,b){
+
+            return a * b 
+
+} 
+
+function divide(a,b){
+
+                return a / b
+
+} 
+
+let number = 1
+function increment(n){
+
+                    return (number += n)
+    } 
+
+let numbers = 1
+function decrement (n){
+
+                        return (numbers -= n )*-1
+} 
+
+let n = 2.222
+makeInt(n , 10); 
